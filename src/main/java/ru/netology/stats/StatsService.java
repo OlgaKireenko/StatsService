be75296790cbb8db3a5d0061ce8017c5c8acf82c
Kireenko Olga <kireenko86.osil@mail.ru>
2021-04-19 13:33:38 +0500

    add code

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,79 @@
+package ru.netology.stats;
+// Сумма всех элементов массива
+public class StatsService {
+    public int calculateSum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum = sum + value;
+        }
+        return sum;
+    }
+
+    //Среднее арифметическое элементов масиива
+    public int calculateAver(int[] values) {
+        int average = calculateSum(values) / values.length;
+        return average;
+    }
+
+    //Последний Месяц максимальных продаж
+    public int maxPurchaisesMonth(int[] values) {
+        int month = 0;
+        int maxMonth = values[0];
+        for (int value : values) {
+            month = month + 1;
+            if (maxMonth < value) {
+                maxMonth = value;
+
+            }
+        }
+        return month;
+    }
+
+    // Последний месяц минимальных продаж
+    public int minPurchaisesMonth(int[] values) {
+        int month = 0;
+        for (int value : values) {
+            int minMonth = values[0];
+            month = month + 1;
+            if (minMonth > value) {
+                minMonth = value;
+
+            }
+        }
+        return month;
+    }
+
+    //Количество месяцев с продажами ниже среднего
+    public int countMinPurchaisesMonth(int[] values) {
+        //int average = calculateSum(values) / values.length;
+        int month = 0;
+        int average = 15;
+        for (int i = 0; i < values.length; i++) {
+            int minMonth = values[0];
+            if (minMonth < average) {
+                //maxMonth=value;
+                month = month + 1;
+            }
+        }
+
+        return month;
+    }
+
+    //Количество месяцев с продажами выше среднего
+    public int countMaxPurchaisesMonth(int[] values) {
+        //int average = calculateSum(values) / values.length;
+        int month = 0;
+        int average = 15;
+
+        for (int i = 0; i < values.length; i++) {
+            int maxMonth = values[i];
+            if (maxMonth > average) {
+
+                //maxMonth=value;
+                month = month + 1;
+            }
+        }
+
+        return month;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,72 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+//Сумма элементов массива
+    @org.junit.jupiter.api.Test
+    void shouldcalculateSum() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        // вызываем целевой метод:
+        long actual = service.calculateSum(values);
+        assertEquals(expected, actual);
+    }
+//Среднее элементов массива
+    @org.junit.jupiter.api.Test
+    void shouldcalculateAverage() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        // вызываем целевой метод:
+        long actual = service.calculateAver(values);
+        assertEquals(expected, actual);
+    }
+//Номер месяца с максимальными продажами
+    @org.junit.jupiter.api.Test
+    void shouldFindNumberMaxpurchaisesMonth() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        // вызываем целевой метод:
+        int actual = service.maxPurchaisesMonth(values);
+        assertEquals(expected, actual);
+    }
+//Номер месяца с минимальными продажами
+    @org.junit.jupiter.api.Test
+    void shouldFindNumberMinpurchaisesMonth() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        // вызываем целевой метод:
+        long actual = service.countMinPurchaisesMonth(values);
+        assertEquals(expected, actual);
+    }
+    // Количество месяцев с продажами ниже среднего
+    @org.junit.jupiter.api.Test
+    void shouldCountNumberMinpurchaisesMonth() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        // вызываем целевой метод:
+        long actual = service.minPurchaisesMonth(values);
+        assertEquals(expected, actual);
+    }
+    // Количество месяцев с продажами выше среднего
+    @org.junit.jupiter.api.Test
+    void shouldCountNumberMaxpurchaisesMonth() {
+        StatsService service = new StatsService();
+        // подготавливаем данные:
+        int[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        // вызываем целевой метод:
+        long actual = service.maxPurchaisesMonth(values);
+        assertEquals(expected, actual);
+    }
+}
